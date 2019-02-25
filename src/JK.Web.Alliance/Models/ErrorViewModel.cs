8765859f84dffe51026e24f1908278605265b2b7
95572457@qq.com <95572457@qq.com>
2019-02-25 21:14:16 +0800

    联盟站点。

---
+++ src/JK.Web.Alliance/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JK.Web.Alliance.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
