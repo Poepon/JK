d89af8b47e6a6174257bc787ad129449fcba3e4b
95572457@qq.com <95572457@qq.com>
2019-05-13 00:47:37 +0800

    ...

--- src/JK.Application/Payments/Orders/OrderIdGenerator.cs
+++ src/JK.Application/Payments/Orders/OrderIdGenerator.cs
@@ -1,42 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using Abp.Dependency;
 
 namespace JK.Payments.Orders
 {
     public class OrderIdGenerator : ISingletonDependency
     {
-        private readonly AbpMemoryCacheManager _memoryCacheManager;
-        private readonly ICacheManager _cacheManager;
-        private readonly ITypedCache<int, List<int>> memoryCache;
-        public OrderIdGenerator(AbpMemoryCacheManager memoryCacheManager, ICacheManager cacheManager)
-        {
-            _memoryCacheManager = memoryCacheManager;
-            _cacheManager = cacheManager;
-            memoryCache = _memoryCacheManager.GetCache<int, List<int>>("PaymentOrderId");
-            memoryCache.DefaultSlidingExpireTime = TimeSpan.FromMilliseconds(10);
-        }
-        private int? serverId;
-        public int GetServerId()
-        {
-            if (!serverId.HasValue)
-            {
-                var servername = Environment.MachineName;
-                var cache = _cacheManager.GetCache<string, List<string>>("WebServers");
-                var list = cache.GetOrDefault("All");
-                if (list == null)
-                {
-                    list = new List<string>();
-                    cache.Set("All", list);
-                }
-                if (!list.Contains(servername))
-                {
-                    list.Add(servername);
-                    cache.Set("All", list);
-                }
-                serverId = list.FindIndex(i => i == servername) + 1;
-            }
-            return serverId.GetValueOrDefault();
-        }
+       
+        public string OrderNoPrefix { get; set; }
+     
         /// <summary>
         /// 生成订单ID
         /// </summary>
@@ -45,36 +17,17 @@ namespace JK.Payments.Orders
         {
             var tid = GenerateTempId();
 
-            return GetServerId() + Clock.Now.ToString("yyMMddHHmmssffff") + tid;
+            return OrderNoPrefix + DateTime.Now.ToString("yyMMddHHmm") + tid;
         }
 
-        private int GenerateTempId()
+        private string GenerateTempId()
         {
-            Random random = new Random();
-            int tid = random.Next(10, 100);
-
-            var key = Clock.Now.Millisecond;
-            var vs = memoryCache.GetOrDefault(key);
-            if (vs == null)
-                vs = new List<int>();
-            if (vs.Count >= 90)
+            int hashCode = Guid.NewGuid().GetHashCode();
+            if (hashCode < 0)
             {
-                Thread.Sleep(1);
-                vs = new List<int>();
+                hashCode = -hashCode;
             }
-            while (IsExists(vs, tid))
-            {
-                tid = random.Next(10, 100);
-            }
-            vs.Add(tid);
-            memoryCache.Set(key, vs);
-            return tid;
+            return string.Format("{0,10:D10}", hashCode) ;
         }
-
-        private bool IsExists(List<int> vs, int tid)
-        {
-            return vs != null && vs.Contains(tid);
-        }
-
     }
 }
