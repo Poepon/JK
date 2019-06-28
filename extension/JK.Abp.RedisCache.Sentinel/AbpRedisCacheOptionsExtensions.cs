813b7d7643cef14f3e057e275adfd3cea1f1ef93
kevin <Chris@ADMIN>
2019-06-28 14:36:27 +0800

    ...

--- extension/JK.Abp.RedisCache.Sentinel/AbpRedisCacheOptionsExtensions.cs
+++ extension/JK.Abp.RedisCache.Sentinel/AbpRedisCacheOptionsExtensions.cs
@@ -6,10 +6,11 @@ namespace JK.Abp.RedisCache.Sentinel
 {
     public static class AbpRedisCacheOptionsExtensions
     {
-        public static void UseSentinel(this AbpRedisCacheOptions options)
+        public static AbpRedisCacheOptions UseSentinel(this AbpRedisCacheOptions options)
         {
             options.AbpStartupConfiguration
                 .ReplaceService<IAbpRedisCacheDatabaseProvider, AbpRedisCacheDatabaseProvider>(DependencyLifeStyle.Singleton);
+            return options;
         }
     }
 }
